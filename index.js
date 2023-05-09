c91365b19d6a82f9adbf8c5768675810700c539e
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-09 10:00:17 +0600

    some error fix

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-//mKFPmEQkbtFHzIO7
-//coffeemaster
+
 
 
 
@@ -37,7 +36,7 @@ async function run() {
 
 
 
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
